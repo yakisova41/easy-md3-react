cda580bb4da83dad10ff41288ede5548eb5162d5
yakisova41 <yakisoba5emon@gmail.com>
2024-12-21 23:22:15 +0900

    Updates

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export { useLit, litToReact } from "./tools";
-export * from "./converted-components";
-export * from "./components/Typography";
-export * from "./components/Theming";
-export * from "./labs/converted-components";
